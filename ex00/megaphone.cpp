64a05c02005bfcdd6b8840cba873e89120f19fa5
Redouan Cherrat <recherra@c2r5p2.1337.ma>
2024-11-12 12:33:20 +0100

    ‎

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -10,10 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <iostream>
 
-
-int main()
+std::string capitalize(std::string str)
 {
-	printf("jack\n");
+	int i = 0;
+	while (str[i])
+	{
+		str[i] = std::toupper(str[i]);
+		i++;
+	}
+	return str;
 }
+
+
+int main(int ac, char **av)
+{
+	int i = 1;
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	while (av[i])
+		std::cout << capitalize(av[i++]);
+	std::cout << std::endl;
+} 
